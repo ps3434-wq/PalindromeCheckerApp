c42b24ac3392f22612523607924c78572418cfd9
ps3434-wq <ps3434@srmist.edu.in>
2026-02-26 15:06:08 +0530

    UC2:Hardcoded palindrome

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,7 +1,21 @@
+import java.sql.SQLOutput;
+
 public class PalindromeCheckerApp {
-  public static void main(String[] args) {
-      System.out.println("welcome to palinfrome checker management system");
-      System.out.println("version :1.0");
-      System.out.println("system initialized successfully.");
-  }
+
+    public static void main(String[] args) {
+
+        String word = "madam";
+        boolean isPalindrome = true;
+
+        for (int i = 0; i < word.length() / 2; i++) {
+
+            if (word.charAt(i) != word.charAt(word.length() - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        System.out.println("Input text: " + word);
+        System.out.println("Is it a Palindrome? : " + isPalindrome);
+    }
 }
